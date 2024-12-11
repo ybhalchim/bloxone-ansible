3e24bf7e2881ccfff52bf90b30a7a72c85d36e2b
ybhalchim <ybhalchim@infoblox.com>
2024-12-11 20:20:37 +0530

    modified ipam_address tests

--- plugins/modules/ipam_address_info.py
+++ plugins/modules/ipam_address_info.py
@@ -31,16 +31,6 @@ options:
             - Filter query to filter objects
         type: str
         required: false
-    inherit:
-        description:
-            - Return inheritance information
-        type: str
-        required: false
-        choices:
-            - full
-            - partial
-            - none
-        default: full
     tag_filters:
         description:
             - Filter dict to filter objects by tags
@@ -414,7 +404,6 @@ def main():
         id=dict(type="str", required=False),
         filters=dict(type="dict", required=False),
         filter_query=dict(type="str", required=False),
-        inherit=dict(type="str", required=False, choices=["full", "partial", "none"], default="full"),
         tag_filters=dict(type="dict", required=False),
         tag_filter_query=dict(type="str", required=False),
     )
